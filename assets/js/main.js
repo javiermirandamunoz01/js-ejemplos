b4b93a6c7e9ebdeb9fc97ddf99a5fe656d510152
javiermirandamunoz01 <javiermirandamunoz01@gmail.com>
2026-02-21 01:06:45 +0000

    aprendiendo a usar java script

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+//let:define varialbles con alcance de bloque y valor reasignable
+// 
+
+//var:ya no se usa
+let nombre = "Javier Miranda"
+
+let saludo = "hola"
+
+
+//esto es una variable constante no cambia
+const pi = 3.141516
+
+
+//este console.log manda al navegador lo que le pasemos
+console.log(nombre)
+
+//tipo de datos
+//esto es texto
+let texto = "esto es un string"
+
+//tipo de dato number
+let numeros = 123456789
+
+//tipo de dato booleano
+// representa si es verdadero o falso
+let verdadero = true
+let falso = false
+
+let esMayor = true
+
+
+//concatenacion
+
+let nombre1 = "javier"
+let nombre2 = "isaias"
+let edad = 33
+
+
+console.log("hola me llamo" +" "+ nombre1 +" "+ nombre2 + " "+"y tengo"+ " " +" "+ edad + " "+ "años");
+
+//operadores matematicos
+let a = 1
+let b = 2
+
+//suma
+console.log(a + b);
+//resta
+console.log(a - b);
+//multiplicacion
+console.log(a * b);
+//division
+console.log(a / b);
+//modulo 
+console.log(a % b);
+
+
+
+
+let h1 = document.getElementById("h1");
+
+h1.textContent ="holaaaa"
+
+
+h1.addEventListener("mouseover",function()h1.style.color = "green"
+{})
